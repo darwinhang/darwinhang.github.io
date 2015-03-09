2f93d3d3b25cf3acbcf9849d36894c2122f3e5ee
winious <darwin.hang@gmail.com>
2015-03-08 23:49:05 -0500

    Add autoprefixer for flexbox to work in Safari

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var autoprefixer =require('gulp-autoprefixer');
 
 // pre-compile less into css
 gulp.task('less', function () {
@@ -14,3 +15,10 @@ gulp.task('minify', function(){
 });
 
 // watcher
+
+// prefix css
+gulp.task('prefix', function(){
+	gulp.src('css/main.css')
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('css'));
+});
